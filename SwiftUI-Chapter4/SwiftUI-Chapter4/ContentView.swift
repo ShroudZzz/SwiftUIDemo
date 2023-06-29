fc9cf3cd5e9fcc594dd438a7c61a73778c5fda74
CL <cl@lbx.dev>
2023-06-29 14:15:18 +0800

    add sources

---
+++ SwiftUI-Chapter4/SwiftUI-Chapter4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Chapter4
+//
+//  Created by LBX-CL on 2023/6/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Chapter4/SwiftUI-Chapter4/SwiftUI_Chapter4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter4App.swift
+//  SwiftUI-Chapter4
+//
+//  Created by LBX-CL on 2023/6/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
